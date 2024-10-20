e3d5833e56992764abb6a27e09daf7de06aa33c0
Andrey Myagkov <andreyoren@gmail.com>
2024-10-20 22:00:03 +0500

    airports css, class

---
+++ js/controls/airport.js
@@ -0,0 +1,46 @@
+const airports = [
+    {
+        countryName: 'Germany',
+        groups: [
+            {
+                name: "Запад",
+                value: "_",
+                items: [
+                    {
+                        name: "Berlin",
+                        value: "bre"
+                    },
+                    {
+                        name: "Dresden",
+                        value: "dre"
+                    }
+                ]
+            }
+        ]
+    }
+];
+
+/**
+ * Класс уповления контролом выбора аэропортов
+ */
+class Airport {
+    constructor(selector) {
+        this.$root = document.querySelector(selector);
+        this.init();
+    }
+    init() {
+        this.render();
+        this.addEventListener();
+    }
+    render() {
+        let html = '';
+        airports.forEach(country => {
+            const countryHtml = '';
+            
+        })
+
+    }
+    addEventListener() {
+
+    }
+}
